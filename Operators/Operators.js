42925ad8648fe0a395fb344f452018255ee25722
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2024-12-30 16:03:51 +0700

    Updated in the operator handling section

--- Operators/Operators.js
+++ Operators/Operators.js
@@ -1,20 +1,23 @@
 // Toán tử số học (Arithmetic Operators):
 let a=5;let b=6;
-console.log(a+b);
-console.log(a*b);
+console.log("Toán tử số học",a + b);
+console.log("Toán tử số học",a - b);
+console.log("Toán tử số học",a % b);
+console.log("Toán tử số học",a * b);
 //Toán tử gán (Assignment Operators):
 let x = 5;
-x += 9;
-console.log(x);
+x += 9;// tương tự như -=,*=,/=
+console.log("Toán tử gán",x);
 //Toán tử so sánh (Comparison Operators):
-console.log(5 == "5");  //so sánh kiểu giá trị 
-console.log(5 === "5"); // so sánh kiểu giá trị và dữ liệu
+console.log("Toán tử so sánh",5 == "5");  //so sánh kiểu giá trị 
+console.log("Toán tử so sánh",5 === "5"); // so sánh kiểu giá trị và dữ liệu
 //Toán tử logic (Logical Operators):
-let c = true, d = false;
-console.log(c && d); //Và (AND)
-console.log(c || d); // Hoặc (OR)
-console.log(!c);//Phủ định (NOT)
+let c = 5;
+console.log("Toán tử logic(AND) :" , c > 0 && c < 10);//Và (AND) Đúng nếu cả hai điều kiện đúng.
+console.log("Toán tử logic(OR) :", c > 0 || c < 10); // Hoặc (OR) Đúng nếu ít nhất một điều kiện đúng
+console.log("Toán tử logic(NOT) :", !(c>10)); //Phủ định (NOT) Đảo ngược giá trị logic.
+
 //Toán tử điều kiện (Conditional Operator - Ternary):
-let age = 16;
+let age = 12;
 let status = (age >= 18) ? "Adult" : "Minor";// toán tử ba ngôi
-console.log(status); 
+console.log("Toán tử điều kiện: ",status); 
